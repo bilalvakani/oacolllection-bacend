eef764e2cffae4aced872482323ad82044a32c11
Bilal Vakani <bilalvakani123@gmail.com>
2026-05-11 04:27:12 +0500

    Fix Cannot GET / on Vercel

--- server.js
+++ server.js
@@ -31,7 +31,14 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/upload', uploadRoutes);
 
+// Default route
+app.get('/', (req, res) => {
+  res.send('OA Collection Backend API is running!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
